bc7f6b342a172bb50bb3913a5874496749242ef7
Preet-Singhvi <86655051+Preet-Singhvi@users.noreply.github.com>
2021-09-30 15:09:13 +0530

    Update Cart.js

--- src/Cart.js
+++ src/Cart.js
@@ -1,37 +1,46 @@
 import React from "react";
+import "./Cart.css"
 
-const Cart = ({cartItems}) => {
-    const list=localStorage.getItem('setCartItems')
-    JSON.parse(list)
-    return ( <div>
-        {console.log(cartItems)}
-        <div className="cart-items">          
+const Cart = ({ cartItems , handleAddProducts , handleRemove , handleAdd , handleReduce}) => {
+    const list=JSON.parse(localStorage.getItem('setCartItems')) ?? [];
+    
+    const totalPrice = list.reduce((price,object) => price + object.qty * object.price ,0)
+
+    return ( 
+      <div className="cart-items">          
+        <div className="cart-items-section">
           <div className="cart-items-header">
             Cart Items
-          </div>
           {list.length === 0 && (
             <div className="cart-items-empty">
                No items are added
             </div>
            )}
-           <div>
-            
-
-           {list.map((product) => {
-             const {id, name, image ,price} = product;
-            <h1>ujcvtfu</h1>
-            console.log(product)
-            return(
-              <article key={product.id} className="cart-items-list">
-               
+          </div>
+          <div>
+           {list.map((objects) => {
+             const {id, name, image ,price,qty} = objects;
+             return(
+               <div key={id} className="cart-items-list">
                 <img src={image} alt={name} />
-              </article>
-            
+                <h4>{name}</h4>
+                <div className="cart-items-button">
+                <button onClick= {() => handleReduce( objects ) }>-</button>
+                <h4>{qty}</h4>
+                <button onClick={() => handleAdd( objects ) }>+</button>
+                </div>
+                <h4>Rs.{price}</h4>
+                <button onClick={() => handleRemove( objects )}>Remove Item</button>
+              </div>           
             );
           })}
-      </div>
-    </div>
+          <div className="payment">
+            <h2>Total Price:{totalPrice}</h2>
+            <a href="/payment">Pay Now</a>
           </div>
+          </div>    
+      </div>
+      </div>
      );
 }
  
